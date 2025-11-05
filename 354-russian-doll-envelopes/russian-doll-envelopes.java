11e2f6efb73f6ca5be6e9821e5ba934153ab07ab
ayushawasthi <devkali849@gmail.com>
2025-11-05 19:40:38 +0530

    Time: 60 ms (8.69%) | Memory: 105.1 MB (7.83%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxEnvelopes(int[][] arr) {
+        Arrays.sort(arr,(x,y)->x[0]!=y[0]?x[0]-y[0]:y[1]-x[1]);
+        int n = arr.length;
+        int height[] = new int[n];
+        for(int i=0;i<n;i++){
+            height[i] = arr[i][1];
+        }
+
+        List<Integer> lis =new ArrayList<>();
+        for(int ele: height){
+            int ind = Collections.binarySearch(lis,ele);
+            ind = -(ind+1);
+            if(ind<0) continue;
+            if(ind == lis.size()) lis.add(ele);
+            else lis.set(ind,ele);
+        }
+
+        return lis.size();
+    }
+}
+
